737f0bc65890b909e960c4184e4e33ff88ed7aec
onulee <onulee@naver.com>
2025-10-28 10:19:52 +0900

    list수업

---
+++ p1028/p02_list.py
@@ -0,0 +1,17 @@
+a_list = [1,2,3,4,5,6,7,8,9]
+print(a_list[1::2])
+print(len(a_list))
+
+a_list = [0,5,3,1,2,4,1,5,6]
+a_list[0] = 100
+print(a_list)
+# 입력한 값이 홀수,짝수인지 출력하시오.
+# if num%2==0 :
+
+
+# str1 = "안녕하세요"
+# print(len(str1))
+# print(str1[1:4])
+# print(str1[::-1])
+# print(a_list[2:5:2])
+
