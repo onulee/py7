12ccae51d47b7d4933a13460c7ac566725cba354
onulee <onulee@naver.com>
2025-10-31 17:43:19 +0900

    영어퀴즈

--- p1031/p07_dict.py
+++ p1031/p07_dict.py
@@ -9,14 +9,30 @@ english = {
 
 # 20문제중에 랜덤으로 5문제를 뽑아서 퀴즈를 만드시오.
 a_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-quest = random.sample(a_list,5)
-quest.sort()
-print(quest) #[0, 11, 12, 14, 17, 19]
-
-for i,k in enumerate(english.keys()):
+quest = random.sample(a_list,5)   # 랜덤5개 - 20문제중 5개를 추출
+quest.sort()                      # 랜덤5개 순서대로 정렬
+quest_dic = {}                    # 정답,오답 입력을 위한 저장공간
+# print(quest) #[0, 11, 12, 14, 17, 19]
+num = 1
+for i,k in enumerate(english.keys()): # index을 함께 추출,key
     if i in quest:
-        print(i,k,english[k])
-
+        # print(i,k,english[k])
+        count = 0
+        # 정답입력
+        print("{} 은(는) 영어로 뭘까요? ".format(k))
+        answer = input(">> ")
+        # 정답확인
+        if answer == english[k]:
+            print("띵동! 정답입니다.")
+            count = count + 1    # count += 1
+            quest_dic[num]  = "정답"   #i+1= 1
+        else:
+            print("오답 : ",english[k])
+            quest_dic[num] = "오답"
+        num = num + 1
+# 1:정답 2:오답 3:오답 4:정답 5:정답
+print(quest_dic)
+print("정답 :",count) 
 
 
 
